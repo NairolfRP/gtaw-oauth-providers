0d555fa5555d05944771dd8368e6a4af0a6f8087
NairolfRP <122038899+NairolfRP@users.noreply.github.com>
2025-06-28 19:34:25 +0200

    chore: base configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: {
+    globals: {
+    ...globals.serviceworker,
+    ...globals.browser,
+      } } },
+  tseslint.configs.recommended,
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+    },
+  },
+  eslintConfigPrettier
+]);
