5a25a2027aadb15342d0dc583f69d254641da612
NairolfRP <122038899+NairolfRP@users.noreply.github.com>
2026-07-18 01:49:08 +0200

    chore: update semantic-release config to trigger a release after a `chore(deps)` commit

--- release.config.mjs
+++ release.config.mjs
@@ -4,4 +4,21 @@
 export default {
   branches: ["main"],
   extends: "semantic-release-monorepo",
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        releaseRules: [
+          {
+            type: "chore",
+            scope: "deps",
+            release: "patch",
+          },
+        ],
+      },
+    ],
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/github",
+  ],
 };
