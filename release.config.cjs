fa9b89740403af5a400ef92f3966056e84aafab2
NairolfRP <122038899+NairolfRP@users.noreply.github.com>
2026-08-10 21:13:17 +0200

    fix(release): remove unused commit analyzer

--- release.config.cjs
+++ release.config.cjs
@@ -8,7 +8,6 @@ module.exports = {
   ],
   extends: "semantic-release-monorepo",
   plugins: [
-    "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/npm",
     "@semantic-release/github",
